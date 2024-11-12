c68d1579b0545bd32dead23f551247534ce9f5f3
hema <hema@HEMA_DHASS>
2024-11-12 22:02:02 +0530

    Third Commit

---
+++ src/com/tnsif/dayfive/singleinheritance/SLInheritance.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayfive.singleinheritance;
+
+public class SLInheritance {
+
+	public static void main(String[] args) {
+		Student s=new Student("HEMA","455545553423","CUDDALORE",9786423951L,44,"IFET COLLEGE OF ENGINEERING");
+		System.out.println(s);
+
+	}
+
+}
